5b9e3fd37a8e7a43789a145521094fa9c1c55afd
lucachaco <luiscarlosch@gmail.com>
2024-04-13 18:59:17 +0200

    add lib folder and first service

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,7 @@
-export default function Home() {
+import { getBlogPosts } from '@lib/blogPosts'
+export default async function Home() {
+    const blogPosts = await getBlogPosts()
+    console.log(blogPosts)
     return <>
 
     </>
